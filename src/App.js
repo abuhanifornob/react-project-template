e9df2f88618297abd69787d5f25c2810ef5e0c54
Abu Hanif <hanifcse@gmail.com>
2023-06-21 16:27:09 +0600

    ness add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { FaYoutube } from 'react-icons/fa';
+
+import { FaFacebookF, FaFacebookMessenger } from 'react-icons/fa';
 
 const App = () => {
     return (
         <div>
-            <header>
-                <h1>A template for React Project</h1>
-            </header>
-            <main>
-                <h1>Welcome to React</h1>
-            </main>
-            <footer>
-                <p className="left">
-                    Subscribe to my{' '}
-                    <a href="https://www.youtube.com/c/anisulislamrubel">
-                        <FaYoutube className="footer__icon" />
-                    </a>{' '}
-                    channel
-                </p>
-                <p className="right">Developed with &hearts; by Anisul Islam</p>
-            </footer>
+            <h2>This is React Icons Componemts</h2>
+            <button>
+                <span className="">
+                    <FaFacebookF className="icon" />
+                </span>
+                <span className="">
+                    <FaFacebookMessenger className="icon" />
+                </span>
+            </button>
+            <button>
+                <span className="">
+                    <FaFacebookMessenger className="icon" />
+                </span>
+            </button>
         </div>
     );
 };
